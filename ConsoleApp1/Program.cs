6c55e36329a4987210ca5ae5eab1e5be931de6c6
Abdelrahmany20 <abdelrahman.yasser.m17@gmail.com>
2025-01-26 10:09:46 +0200

    Q02

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q02
+            //Console.WriteLine("Enter the size :");
+            //int N = int.Parse(Console.ReadLine());
+
+            //Console.Write("Enter the Elements : ");
+            //List<int> list = new List<int>();
+            //for (int i = 0; i < N; i++)
+            //{
+            //    list.Add(int.Parse(Console.ReadLine()));
+            //}
+
+            //List<int> reversedList = new List<int>(list);
+            //reversedList.Reverse();
+
+            //if (list.SequenceEqual(reversedList))
+            //{
+            //    Console.WriteLine("YES");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("NO");
+            //}
+            #endregion        }
         }
     }
-}
